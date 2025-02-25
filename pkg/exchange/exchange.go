7b9681ac5a7536735390b3b5eac6daca381f1898
vansh <vanshpatel1299@gmail.com>
2025-02-25 20:46:57 +0530

    Fun plate added

--- pkg/exchange/exchange.go
+++ pkg/exchange/exchange.go
@@ -2,28 +2,39 @@ package exchange
 
 import (
 	"dummyengine/pkg/orderbook"
-	"math/big"
-	"strings"
-
 	"github.com/streadway/amqp"
+	"math/big"
 )
 
 type Exchange struct {
-	eventID *big.Int
-    OrderBooks map[string]*orderbook.OrderBook
+	eventID        *big.Int
+	OrderBooks     map[string]*orderbook.OrderBook
 	TradeQueue     string
 	PriceQueue     string
 	OrderBookQueue string
 }
 
-
 func NewExchange(ch *amqp.Channel, tradeQueue, priceQueue, orderBookQueue string) *Exchange {
-
 	return &Exchange{
-		eventID: big.Int,
-		OrderBooks: nil,
-		tradeQueue: tradeQueue,
-		priceQueue: priceQueue,
-		orderBookQueue: orderBookQueue
+		eventID:        big.NewInt(0),
+		OrderBooks:     make(map[string]*orderbook.OrderBook),
+		TradeQueue:     tradeQueue,
+		PriceQueue:     priceQueue,
+		OrderBookQueue: orderBookQueue,
 	}
+}
+
+func (e *Exchange)AddEvent{
+
+}
+
+func (e *Exchange)Settlement{
+	
+}
+
+func (e *Exchange)AddBuyOrder{
+	
+}
+func (e *Exchange)AddSellOrder{
+	
 }
